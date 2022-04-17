2d12e35451e2ff8d58de8584ab96f57e27c4975d
yabuszko <74075921+yabuszko@users.noreply.github.com>
2022-04-17 19:24:07 +0200

    New things
    Zmiany

--- Double Pendulum/main.cpp
+++ Double Pendulum/main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
-#include <iostream>
+#include <sstream>
 #include <vector>
 
 #include "settings.h"
@@ -24,8 +24,24 @@ public:
 		target->draw(*this);
 	}
 
+	void onHold(Vector2f pos, bool p) {
+		if (getGlobalBounds().contains(lastpos)) { // mouse over
+			if (p) {
+				this->position = static_cast<Vector2f>(Mouse::getPosition());
+				setPosition(this->position);
+
+				set_new_values = true;
+			}
+			else
+				set_new_values = false;
+		}
+
+		lastpos = static_cast<Vector2f>(Mouse::getPosition());
+	}
+
 private:
 	Vector2f position;
+	Vector2f lastpos = static_cast<Vector2f>(Mouse::getPosition());
 
 public:
 	float mass;
@@ -33,13 +49,15 @@ public:
 
 class Line :public RectangleShape {
 public:
-	Line(Vector2f pos, float width, float angle) {
+	Line(Vector2f pos, float width, float angle, float len) {
 		this->thickness = width;
 		this->position = pos;
 		this->rotation = angle;
 
 		this->velocity = 0;
 		this->acceleration = 0;
+
+		this->length = len;
 
 		setPosition(this->position);
 		setSize(Vector2f(this->thickness, this->length));
@@ -60,16 +78,13 @@ public:
 		setRotation(new_angle * 180/pi);
 	}
 
-	const float getLength() const {
-		return this->length;
-	}
-
 	void show(RenderWindow* target) const {
 		target->draw(*this);
 	}
-private:
-	const float length = 200;
+public:
+	float length;
 
+private:
 	float thickness;
 	float rotation;
 	Vector2f position;
@@ -137,17 +152,55 @@ int main() {
 	Vector2f translated = pivot.getPosition();
 
 	// Other objects
-	Line line1(pivot.getPosition(), 1.f, -pi/2);
+	Line line1(pivot.getPosition(), 1.f, -pi/2, len1);
 
 	unsigned int faster = 0;
+	bool freetransform = false;
+	bool pressed = false;
 
-	Ball m1(ma1 / 2, Color(0, 255, 0, 100), Vector2f(translated.x + line1.getLength() * sin(-line1.getRotation() * pi / 180),
-		translated.y + line1.getLength() * cos(line1.getRotation() * pi / 180)), ma1);
+	Ball m1(ma1 / 2, Color(45, 129, 224, 100), Vector2f(translated.x + line1.length * sin(-line1.getRotation() * pi / 180),
+		translated.y + line1.length * cos(line1.getRotation() * pi / 180)), ma1);
 
-	Line line2(m1.getPosition(), 1.f, -pi/2);
+	Line line2(m1.getPosition(), 1.f, -pi/2, len2);
 
-	Ball m2(ma2 / 2, Color(0, 255, 0, 100), Vector2f(m1.getPosition().x + line2.getLength() * sin(-line2.getRotation() * pi / 180),
-		m1.getPosition().y + line2.getLength() * cos(line2.getRotation() * pi / 180)), ma2);
+	Ball m2(ma2 / 2, Color(0, 255, 0, 100), Vector2f(m1.getPosition().x + line2.length * sin(-line2.getRotation() * pi / 180),
+		m1.getPosition().y + line2.length * cos(line2.getRotation() * pi / 180)), ma2);
+
+	// Fonts
+	Font font;
+	if(!font.loadFromFile("Gilroy-Light.otf")) return -1;
+
+	Text text1, text2, text3, text4, text5, text6;
+	text1.setFont(font);
+	text1.setCharacterSize(20);
+	text1.setPosition(0, 0);
+
+	text2.setFont(font);
+	text2.setCharacterSize(20);
+	text2.setFillColor(Color(45, 129, 224, 100));
+	text2.setPosition(0, 25);
+
+	text3.setFont(font);
+	text3.setCharacterSize(20);
+	text3.setFillColor(Color(0, 255, 0, 100));
+	text3.setPosition(0, 50);
+
+	text4.setFont(font);
+	text4.setCharacterSize(20);
+	text4.setFillColor(Color(45, 129, 224, 100));
+	text4.setPosition(0, 75);
+
+	text5.setFont(font);
+	text5.setCharacterSize(20);
+	text5.setFillColor(Color(0, 255, 0, 100));
+	text5.setPosition(0, 100);
+
+	text6.setFont(font);
+	text6.setCharacterSize(10);
+	text6.setPosition(0, VideoMode::getDesktopMode().height - 10);
+
+	stringstream stext1, stext2, stext3, stext4, stext5;
+
 
 	// Trace
 	Trace trace_1(&canvas);
@@ -165,43 +218,136 @@ int main() {
 					if (faster == 0) faster = 1;
 					else faster = 0;
 				}
+				else if (e.key.code == Keyboard::T) {
+					freetransform = !freetransform;
+				}
+				else if (e.key.code == Keyboard::Q) {
+					if(freetransform)
+					if (m1.mass >= 10) {
+						m1.mass -= 5;
+						m1.setRadius(m1.getRadius() - 2.5);
+						m1.setOrigin(m1.getRadius(), m1.getRadius());
+					}
+				}
+				else if (e.key.code == Keyboard::W) {
+					if (freetransform) {
+						if (m1.mass <= 100) {
+							m1.mass += 5;
+							m1.setRadius(m1.getRadius() + 2.5);
+							m1.setOrigin(m1.getRadius(), m1.getRadius());
+						}
+					}
+				}
+				else if (e.key.code == Keyboard::A) {
+					if (freetransform)
+					if (m2.mass >= 10) {
+						m2.mass -= 5;
+						m2.setRadius(m2.getRadius() - 2.5);
+						m2.setOrigin(m2.getRadius(), m2.getRadius());
+					}
+				}
+				else if (e.key.code == Keyboard::S) {
+					if (freetransform)
+					if (m2.mass <= 100) {
+						m2.mass += 5;
+						m2.setRadius(m2.getRadius() + 2.5);
+						m2.setOrigin(m2.getRadius(), m2.getRadius());
+					}
+				}
+				else if (e.key.code == Keyboard::C) {
+					canvas.clear(Color::Black);
+					trace_1.get_data(m1.getPosition());
+					trace_2.get_data(m2.getPosition());
+				}
+			}
+			else if (e.type == Event::MouseButtonPressed) {
+				pressed = true;
+			}
+			else if (e.type == Event::MouseButtonReleased) {
+				pressed = false;
 			}
 		}
-		
+
 		// update
-		//line1.setRotation(line1.getRotation() + 0.5);
-		//line2.setRotation(line2.getRotation() - 0.3);
-		for (size_t i = 0; i < 1 + speed_factor * faster; i++) {
-			float num1 = -gravitational_acceleration * (2 * m1.mass + m2.mass) * sin(line1.getRotation() * pi / 180);
-			float num2 = -m2.mass * gravitational_acceleration * sin((line1.getRotation() - 2 * line2.getRotation()) * pi / 180);
-			float num3 = -2 * sin((line1.getRotation() - line2.getRotation()) * pi / 180);
-			float num4 = line2.velocity * line2.velocity * line2.getLength() + line1.velocity * line1.velocity * line1.getLength() *
-				cos((line1.getRotation() - line2.getRotation()) * pi / 180);
-			float den = line1.getLength() * 2 * (m1.mass + m2.mass - m2.mass * cos((2 * line1.getRotation() - 2 * line2.getRotation()) * pi / 180));
-			line1.acceleration = (num1 + num2 + num3 * num4) / den;
+		stext1.str(string()); stext2.str(string()); stext3.str(string()); stext4.str(string()); stext5.str(string());
 
-			num1 = 2 * sin((line1.getRotation() - line2.getRotation()) * pi / 180);
-			num2 = line1.velocity * line1.velocity * line1.getLength() * (m1.mass + m2.mass);
-			num3 = gravitational_acceleration * (m1.mass + m2.mass) * cos(line1.getRotation() * pi / 180);
-			num4 = line2.velocity * line2.velocity * line2.getLength() * m2.mass * cos((line1.getRotation() - line2.getRotation()) * pi / 180);
-			den = line2.getLength() * 2 * (m1.mass + m2.mass - m2.mass * cos((2 * line1.getRotation() - 2 * line2.getRotation()) * pi / 180));
-			line2.acceleration = num1 * (num2 + num3 + num4) / den;
+		if (!freetransform) {
+			for (size_t i = 0; i < 1 + speed_factor * faster; i++) {
+				float num1 = -gravitational_acceleration * (2 * m1.mass + m2.mass) * sin(line1.getRotation() * pi / 180);
+				float num2 = -m2.mass * gravitational_acceleration * sin((line1.getRotation() - 2 * line2.getRotation()) * pi / 180);
+				float num3 = -2 * sin((line1.getRotation() - line2.getRotation()) * pi / 180);
+				float num4 = line2.velocity * line2.velocity * line2.length + line1.velocity * line1.velocity * line1.length *
+					cos((line1.getRotation() - line2.getRotation()) * pi / 180);
+				float den = line1.length * 2 * (m1.mass + m2.mass - m2.mass * cos((2 * line1.getRotation() - 2 * line2.getRotation()) * pi / 180));
+				line1.acceleration = (num1 + num2 + num3 * num4) / den;
 
-			line1.update();
-			line2.update();
+				num1 = 2 * sin((line1.getRotation() - line2.getRotation()) * pi / 180);
+				num2 = line1.velocity * line1.velocity * line1.length * (m1.mass + m2.mass);
+				num3 = gravitational_acceleration * (m1.mass + m2.mass) * cos(line1.getRotation() * pi / 180);
+				num4 = line2.velocity * line2.velocity * line2.length * m2.mass * cos((line1.getRotation() - line2.getRotation()) * pi / 180);
+				den = line2.length * 2 * (m1.mass + m2.mass - m2.mass * cos((2 * line1.getRotation() - 2 * line2.getRotation()) * pi / 180));
+				line2.acceleration = num1 * (num2 + num3 + num4) / den;
 
-			m1.setPosition(Vector2f(translated.x + line1.getLength() * sin(-line1.getRotation() * pi / 180),
-				translated.y + line1.getLength() * cos(line1.getRotation() * pi / 180)));
-			m2.setPosition(Vector2f(m1.getPosition().x + line2.getLength() * sin(-line2.getRotation() * pi / 180),
-				m1.getPosition().y + line2.getLength() * cos(line2.getRotation() * pi / 180)));
-			line2.setPosition(m1.getPosition());
+				line1.update();
+				line2.update();
 
-			trace_1.leave_dot(m1.getPosition(), Color(13, 128, 43));
-			trace_2.leave_dot(m2.getPosition(), Color(72, 217, 109));
-			canvas.display();
+				m1.setPosition(Vector2f(translated.x + line1.length * sin(-line1.getRotation() * pi / 180),
+					translated.y + line1.length * cos(line1.getRotation() * pi / 180)));
+				m2.setPosition(Vector2f(m1.getPosition().x + line2.length * sin(-line2.getRotation() * pi / 180),
+					m1.getPosition().y + line2.length * cos(line2.getRotation() * pi / 180)));
+				line2.setPosition(m1.getPosition());
+
+				trace_1.leave_dot(m1.getPosition(), Color(30, 103, 186));
+				trace_2.leave_dot(m2.getPosition(), Color(72, 217, 109));
+				canvas.display();
+			}
+		}
+		else{
+			m1.onHold(static_cast<Vector2f>(Mouse::getPosition()), pressed);
+			m2.onHold(static_cast<Vector2f>(Mouse::getPosition()), pressed);
+
+			if (set_new_values) {
+				line1.length = sqrt(pow(m1.getPosition().y - pivot.getPosition().y, 2) + pow(m1.getPosition().x - pivot.getPosition().x, 2));
+				line1.setSize(Vector2f(line1.getSize().x, line1.length));
+				len1 = line1.length;
+
+				float angle1 = asin((m1.getPosition().x - pivot.getPosition().x) / len1);
+				if (m1.getPosition().y >= line1.getPosition().y)
+					line1.setRotation(-angle1 * 180 / pi);
+				else { // arcsin(x) = <-pi/2, pi/2>
+					line1.setRotation(180 + angle1 * 180 / pi); // pi/2
+				}
+
+				line2.setPosition(m1.getPosition());
+				line2.length = sqrt(pow(m2.getPosition().y - m1.getPosition().y, 2) + pow(m2.getPosition().x - m1.getPosition().x, 2));
+				line2.setSize(Vector2f(line2.getSize().x, line2.length));
+				len2 = line2.length;
+
+				float angle2 = asin((m2.getPosition().x - m1.getPosition().x) / len2);
+				if (m2.getPosition().y >= line2.getPosition().y)
+					line2.setRotation(-angle2 * 180 / pi);
+				else { // arcsin(x) = <-pi/2, pi/2>
+					line2.setRotation(180 + angle2 * 180 / pi); // pi/2
+				}
+
+				canvas.clear(Color::Black);
+				trace_1.get_data(m1.getPosition());
+				trace_2.get_data(m2.getPosition());
+			}
 		}
 
-		//cout << line1.getPosition().x << " " << line1.getPosition().y <<  " " << line1.acceleration << endl;
+		stext1 << "g: 0.1 m/s^2";
+		stext2 << "T -> (Q/W) m1: " << m1.mass << " kg";
+		stext3 << "T -> (A/S) m2: " << m2.mass << " kg";
+		stext4 << "r1: " << trunc(line1.length);
+		stext5 << "r2: " << trunc(line2.length);
+
+		text1.setString(stext1.str());
+		text2.setString(stext2.str());
+		text3.setString(stext3.str());
+		text4.setString(stext4.str());
+		text5.setString(stext5.str());
+		text6.setString("by Oliwier Moskalewicz 2022");
 
 		// draw
 		window.clear(Color::Black);
@@ -217,6 +363,13 @@ int main() {
 		pivot.show(&window);
 		m1.show(&window);
 		m2.show(&window);
+
+		window.draw(text1);
+		window.draw(text2);
+		window.draw(text3);
+		window.draw(text4);
+		window.draw(text5);
+		window.draw(text6);
 
 		window.display();
 		
--- Double Pendulum/settings.h
+++ Double Pendulum/settings.h
@@ -1,23 +1,28 @@
 #pragma once
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 float ma1 = 40;
 float ma2 = 20;
 
+float len1 = 200;
+float len2 = 200;
+
 bool to_rest = false;
+bool set_new_values = false;
 
 size_t speed_factor = 5;
 
 const float pi = 3.1415927;
-const float gravitational_acceleration = 0.06;
+const float gravitational_acceleration = 0.1;
 
 void read_data() {
 	string line;
 	ifstream file("dane.txt");
 	if (file.is_open()) {
 		
-		while (file >> ma1 >> ma2 >> to_rest) {
+		while (file >> ma1 >> ma2 >> len1 >> len2 >> to_rest) {
 			cout << ma1 << " " << ma2 << " " << to_rest << endl;
 		}
 		file.close();
